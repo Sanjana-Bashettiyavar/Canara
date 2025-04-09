5796c51a1861e17dd30795b60e76b5d5cc09f230
sanjana <144414788+Sanjana-Bashettiyavar@users.noreply.github.com>
2025-04-10 00:35:48 +0530

    Update 1 App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hii");
+         System.out.println("hello");
     }
 }
